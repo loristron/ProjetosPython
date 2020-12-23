83de746eada8bf5aaeff6a23125b0b09abe4dd42
loristron <loremmiranda@gmail.com>
2020-12-23 00:35:13 -0300

    POO + GUI

--- DecidaPorMim/decida_por_mim.py
+++ DecidaPorMim/decida_por_mim.py
@@ -6,20 +6,25 @@ import PySimpleGUI as sg
 class Decisao:
     def __init__(self):
         self.respostas = ["Sim", "Não", "Talvez", "Que pergunta é essa?!", "Talvez na próxima vida", "Nem a pau", "Jamé", "Demais da conta", "Com certeza!", "Prefiro não comentar.", "Tá doido"]
-    
+        self.rodando = True
+
     def Iniciar(self):  
         layout = [
             [sg.Text("Faça sua pergunta:")],
             [sg.Input(size=(40,0), key="campo")],
             [sg.Button("Vai!")],
-            [sg.Output(size=(40,0))]
+            [sg.Output(size=(40,0))],
+            [sg.Button("Sair")]
         ]
         self.janela = sg.Window("Vamo conversar", layout=layout)
         try:
-            while True:
+            while self.rodando == True:
                 self.eventos, self.valores = self.janela.Read()
                 if self.eventos == 'Vai!':
                     print(rd.choice(self.respostas))
+                if self.eventos == 'Sair':
+                    self.rodando = False
+                    self.janela.Close()
         except:
             print("Deu erro!")
 
